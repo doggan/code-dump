36b0f3990e19687d6877d6b2a9b1c0b481e1efac
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-20 18:43:51 -0400

    Import.

---
+++ algorithm/binary_search.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace Hoge
+{
+	class MainClass
+	{
+		static int search(int[] list, int val)
+		{
+			if (list == null || list.Length == 0) {
+				return -1;
+			}
+
+			return searchImpl(list, val, 0, list.Length - 1);
+		}
+
+		static int searchImpl(int[] list, int val, int min, int max)
+		{
+			if (min > max) {
+				return -1;
+			}
+
+			int m = midpoint(min, max);
+			int mVal = list[m];
+			if (val < mVal) {
+				return searchImpl(list, val, min, m - 1);
+			}
+			else if (val > mVal) {
+				return searchImpl(list, val, m + 1, max);
+			}
+			else {
+				return m;
+			}
+		}
+
+		static int midpoint(int min, int max)
+		{
+			return min + (max - min) / 2;
+		}
+
+		private static  int _count = 1;
+		static void runCase(int[] list, int val, int expected)
+		{
+			int actual = search(list, val);
+			if (expected == actual) {
+				Console.WriteLine("{0}: Success!", _count);
+			} else {
+				Console.WriteLine("{0}: Failure! {1} != {2}", _count, expected, actual);
+			}
+			++_count;
+		}
+
+		public static void Main (string[] args)
+		{
+			// Driver program.
+			runCase(null, 1, -1);
+			runCase(new int[]{}, 1, -1);
+			runCase(new int[]{1, 2, 3}, 3, 2);
+			runCase(new int[]{1, 2, 3}, 6, -1);
+			runCase(new int[]{1, 2, 3, 8, 9}, 8, 3);
+			runCase(new int[]{1, 2, 3, 8, 9}, 1, 0);
+		}
+	}
+}
